b1c04469b4c7220aa583647be71d961c316b40aa
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-12 18:06:31 +0200

    Add files via upload

---
+++ lab_env.sh
@@ -0,0 +1,7 @@
+#export variables environment
+export DT_TENANT_URL="https://abcd.live.dynatrace.com"
+export DT_API_TOKEN="XXX"
+#export variables application
+export Appname="easytravelxx"
+export Hostname="yyy.zzz.dynatrace.com"
+export Email="myemail@email.com"
