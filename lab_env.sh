039a0d6ab32a240744698130b8af914a0289b38c
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-22 12:53:56 +0200

    Delete lab_env.sh

--- lab_env.sh
+++
@@ -1,9 +0,0 @@
-#export variables environment
-export DT_TENANT_URL="https://abcd.live.dynatrace.com"
-export DT_API_TOKEN="XXX"
-#unique id
-export config="config-id"
-#export variables application
-export Appname="easytravelxx"
-export Hostname="yyy.zzz.dynatrace.com"
-export Email="myemail@email.com"
