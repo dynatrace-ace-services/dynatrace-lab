fb67dc6112a54d29bca2cc043929f52f91de6f5b
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-22 13:39:43 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -7,6 +7,8 @@ export config="config-id"
 export HostGroup="easytravelxx"
 export DonainName="yyy.zzz.dynatrace.com"
 export Email="myemail@email.com"
-#synthetic location
+#synthetic location (for live.dynatrace.com)
+export PublicSyntheticLocation1="GEOLOCATION-8A751DADED5D705A" #Marseille_Azure
+export PublicSyntheticLocation2="GEOLOCATION-E266126A762728A2" #Paris_AWS
 
 
