77591c09a0fa6cdbeffa30e80d94518e08e394d0
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-22 11:05:21 +0200

    Update lab_env.sh

--- lab_env.sh
+++ lab_env.sh
@@ -1,9 +1,9 @@
 #export variables environment
 export DT_TENANT_URL="https://abcd.live.dynatrace.com"
 export DT_API_TOKEN="XXX"
+#unique id
+export config-id=config-id
 #export variables application
 export Appname="easytravelxx"
 export Hostname="yyy.zzz.dynatrace.com"
 export Email="myemail@email.com"
-#type training (if user training => true, if admin training => false) 
-export Fullconfig=false
