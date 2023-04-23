68bf2f2e71594ab1c3735a46b7a11f63a1ebe878
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-23 11:17:55 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,6 @@
 #export variables environment
 export DT_TENANT_URL="https://abcd.live.dynatrace.com"
 export DT_API_TOKEN="XXX"
-#unique id
-export config="config-id"
 #export variables application
 export HostGroup="easytravelxx"
 export DonainName="yyy.zzz.dynatrace.com"
