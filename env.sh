8668600ca027a472e1f372459dfbabb2638c18ca
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-24 11:32:14 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -8,5 +8,3 @@ export Email="myemail@email.com"
 #synthetic location (for live.dynatrace.com)
 export PublicSyntheticLocation1="GEOLOCATION-E266126A762728A2" #Paris_AWS
 export PublicSyntheticLocation2="GEOLOCATION-8A751DADED5D705A" #Marseille_Azure
-
-
